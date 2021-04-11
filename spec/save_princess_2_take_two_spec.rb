f03828d5b392ef0cafab5ed8f5aab4655b87ccf0
fieldstyler <tdfields93@gmail.com>
2021-04-11 15:27:24 -0600

    Use initial game_board class instead of making a game_board2 class.

--- spec/save_princess_2_take_two_spec.rb
+++ spec/save_princess_2_take_two_spec.rb
@@ -3,7 +3,7 @@ require './lib/game_board'
 
 RSpec.describe "Save Princess" do
   it "Checks that we can successfully create a grid" do
-    game_board = GameBoard2.new(n: 3, p_location: [2, 2], m_location: [0, 1], grid: [["-m-"], ["---"], ["--p"]])
+    game_board = GameBoard.new(n: 3, p_location: [2, 2], m_location: [0, 1], grid: [["-m-"], ["---"], ["--p"]])
     expect(game_board.n).to eq(3)
     expect(game_board.p_location).to eq([2, 2])
     expect(game_board.p_location[0]).to eq(2)
@@ -15,8 +15,8 @@ RSpec.describe "Save Princess" do
   end
 
   it "Prints directions to princess given a game board" do
-    game_board1 = GameBoard2.new(n: 5, p_location: [4, 1], m_location: [3, 4], grid: [["-----"], ["-----"], ["-----"], ["----m"], ["-p---"]])
-    game_board2 = GameBoard2.new(n: 3, p_location: [0, 0], m_location: [2, 0], grid: [["p--"], ["---"], ["m--"]])
+    game_board1 = GameBoard.new(n: 5, p_location: [4, 1], m_location: [3, 4], grid: [["-----"], ["-----"], ["-----"], ["----m"], ["-p---"]])
+    game_board2 = GameBoard.new(n: 3, p_location: [0, 0], m_location: [2, 0], grid: [["p--"], ["---"], ["m--"]])
     expect(game_board1.next_move).to eq("DOWN")
     expect(game_board2.next_move).to eq("UP")
   end
