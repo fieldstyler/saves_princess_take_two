cc188890b79e54f42a8ba81baaa489f94b79dc0c
fieldstyler <tdfields93@gmail.com>
2021-04-14 16:42:07 -0600

    Update parameters format and create all new methods in GameBoard class

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,69 +1,52 @@
 class GameBoard
-  attr_reader :n, :p_location, :m_location, :grid
-  def initialize(game_hash)
-    @n = game_hash[:n]
-    @p_location = game_hash[:p_location]
-    @m_location = game_hash[:m_location]
-    @grid = game_hash[:grid]
+  attr_reader :n, :grid, :m_location, :p_location
+  def initialize(n, grid)
+    @n = n
+    @grid = grid
+    @m_location = find_location('m')
+    @p_location = find_location('p')
   end
 
-  def display_path_to_princess
-    p_row = @p_location[0]
-    m_row = @m_location[0]
-    p_column = @p_location[1]
-    m_column = @m_location[1]
-    print_directions_to_princess(p_row, m_row, p_column, m_column)
+  def find_location(letter, coords=[], row_number=0)
+    @grid.each_with_index do |row, index|
+      if row.include?(letter)
+        coords << row_number
+        coords << row.index(letter)
+      end
+      row_number += 1
+    end
+    coords
   end
 
-  def next_move
-    p_row = @p_location[0]
-    m_row = @m_location[0]
-    p_column = @p_location[1]
-    m_column = @m_location[1]
-    print_next_move(p_row, m_row, p_column, m_column)
+  def display_path_to_princess(steps="")
+    steps += directions(@m_location, @p_location)
+    steps
   end
 end
 
-def print_directions_to_princess(p_row, m_row, p_column, m_column)
-  directions = ""
-  if p_row > m_row
-    until p_row == m_row
-      m_row += 1
-      directions += "DOWN\n"
+def directions(m, p)
+  diff = (m[0] - p[0]).abs
+  steps = ""
+  if m[0] > p[0]
+    diff.times do steps += "UP\n"
     end
-  elsif p_row < m_row
-    until p_row == m_row
-      m_row -= 1
-      directions += "UP\n"
+  else
+    diff.times do steps += "DOWN\n"
     end
   end
-  if p_column > m_column
-    until p_column - m_column == 1
-      m_column += 1
-      directions += "RIGHT\n"
-    end
-    directions += "RIGHT"
-  elsif p_column < m_column
-    until m_column - p_column == 1
-      m_column -= 1
-      directions += "LEFT\n"
-    end
-    directions += "LEFT"
-  end
-  directions
+  steps += horizontal_directions(m, p)
+  steps
 end
 
-def print_next_move(p_row, m_row, p_column, m_column)
-  if p_row > m_row
-    move = "DOWN"
-  elsif p_row < m_row
-    move = "UP"
-  elsif p_row == m_row
-    if p_column > m_column
-      move = "RIGHT"
-    else
-      move = "LEFT"
+def horizontal_directions(m, p)
+  diff = (m[1] - p[1]).abs
+  steps = ""
+  if m[1] > p[1]
+    diff.times do steps += "LEFT\n"
+    end
+  else
+    diff.times do steps += "RIGHT\n"
     end
   end
-  move
+  steps
 end
